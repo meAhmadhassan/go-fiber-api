894f3e35347413b74c1faf2a4aa8691b0f628915
Your Name <meahmadhassan@users.noreply.github.com>
2025-03-16 01:42:54 +0500

    divide into api module

---
+++ api/api.go
@@ -0,0 +1,117 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/meahmadhassan/go-fiber-api/models"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+// Setup API Routes
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("/delete_book/:id", r.DeleteBook)
+	api.Get("/get_book/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
+}
+
+func (r *Repository) CreateBook(context *fiber.Ctx) error {
+	book := Book{}
+
+	err := context.BodyParser(&book)
+
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
+	}
+
+	err = r.DB.Create(&book).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "unable to create book"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "book created!"})
+
+	return nil
+}
+
+func (r *Repository) DeleteBook(context *fiber.Ctx) error {
+	bookModel := models.Books{}
+	id := context.Params("id")
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	err := r.DB.Delete(bookModel, id)
+
+	if err.Error != nil {
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"message": "could not delete book",
+		})
+		return err.Error
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "book delete successfully",
+	})
+	return nil
+}
+
+
+
+func (r *Repository) GetBookByID(context *fiber.Ctx) error {
+
+	id := context.Params("id")
+	bookModel := &models.Books{}
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	fmt.Println("the ID is", id)
+
+	err := r.DB.Where("id = ?", id).First(bookModel).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get the book"})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "book id fetched successfully",
+		"data":    bookModel,
+	})
+	return nil
+}
+
+
+func (r *Repository) GetBooks(c *fiber.Ctx) error {
+	bookModels := &[]models.Books{}
+
+	if err := r.DB.Find(bookModels).Error; err != nil {
+		return c.Status(http.StatusBadRequest).JSON(&fiber.Map{"message": "No books found"})
+	}
+
+	return c.Status(http.StatusOK).JSON(&fiber.Map{"message": "Books fetched!", "data": bookModels})
+}
